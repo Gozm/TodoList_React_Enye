b3592040ec79b3e2dddb1919f5393f64e0ed5bbd
Gozm <48566356+Gozm@users.noreply.github.com>
2019-03-16 21:50:45 +0100

    Add files via upload

---
+++ precache-manifest.5479c480c6552a0b5b7d9f34dabddb38.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "f3a9996b965c75959643",
+    "url": "/static/js/main.cbb49c1e.chunk.js"
+  },
+  {
+    "revision": "e89b6db1e14376caaecc",
+    "url": "/static/js/2.dd627044.chunk.js"
+  },
+  {
+    "revision": "f3a9996b965c75959643",
+    "url": "/static/css/main.824125dd.chunk.css"
+  },
+  {
+    "revision": "84e389ff7bc659e54e4caadf94dfbc85",
+    "url": "/index.html"
+  }
+];
